6d4a2abc59a03dae09f21e7cb34d1a1586f24cdb
Joshua Palamuttam <joshpal97@gmail.com>
2020-05-21 22:25:03 -0400

    Setting up node server

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { config } from "dotenv"
+if(process.env.NODE_ENV !== "production") {
+    config();
+}
+
+import Stripe from 'stripe'
+export const stripe = new Stripe(process.env.STRIPE_SECRET, {
+    apiVersion: '2020-03-02'
+});
