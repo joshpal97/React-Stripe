92446d38b3dd0d14372edfe6dd25ce84119873a9
Joshua Palamuttam <joshpal97@gmail.com>
2020-05-22 22:28:37 -0400

    added some styling and setup for frontend

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { elements, Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
+export const stripePromise = loadStripe(
+	'pk_test_UEYHhs9qZ96casVHydmc6zLB000IFqLC50',
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Elements stripe={stripePromise}>
+			<App />
+		</Elements>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
