20a55915cec892e3c4444c2bce4da363505e1977
Joshua Palamuttam <joshpal97@gmail.com>
2020-05-23 22:27:45 -0400

    checkout on backend

--- Server/src/api.ts
+++ Server/src/api.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 export const app = express();
 
 app.use(express.json());
@@ -6,8 +6,17 @@ app.use(express.json());
 import cors from 'cors';
 app.use(cors({ origin: true }));
 
-app.post('/test', (req: Request, res: Response) => {
-	const amount = req.body.amount;
+import { createStripeCheckoutSession } from './checkout';
 
-	res.status(200).send({ with_tax: amount * 7 });
-});
+app.post(
+	'/checkouts/',
+	runAsync(async ({ body }: Request, res: Response) => {
+		res.send(await createStripeCheckoutSession(body.line_items));
+	}),
+);
+
+function runAsync(callback: Function) {
+	return (req: Request, res: Response, next: NextFunction) => {
+		callback(req, res, next).catch(next);
+	};
+}
